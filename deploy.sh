ec18ec1a7f2202949854e76a9f95a2c824d28133
Cletus Igwe <me@cletusigwe.com>
2024-10-06 21:09:53 +0100

    Update Content at Sun Oct  6 09:09:53 PM WAT 2024

--- deploy.sh
+++ deploy.sh
@@ -5,10 +5,10 @@ GOOGLE_CHECK=$(curl -Is http://www.google.com | head -n 1)
 if echo "$GOOGLE_CHECK" | grep -q "200"; then # internet is up
     echo "Internet is up"
     set -xe
-    if [ -n "$(git status src/content --porcelain)" ]; then #new content has been added
+    if [ -n "$(git status src/content/microblog --porcelain)" ]; then #new content has been added
         bun run build
         COMMIT_MSG="Update Content at $(date)"
-        git add src/content/*
+        git add "*"
         git commit -m "$COMMIT_MSG"
         git push origin master
     else
